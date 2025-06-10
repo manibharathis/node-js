f40128367c1e9b4d1d41bb009b2fc1536be864c4
manibharathis <remasekar.96@gmail.com>
2025-06-10 10:10:14 +0530

    require and exports

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const {x,xyzfunc}=require('./xyz.js')
+let a= 10;
+let b= 20;
+console.log(a+b);
+console.log(x)
+xyzfunc()
---
+++ xyz.js
@@ -0,0 +1,7 @@
+console.log("xys console")
+const x= 10;
+const xyzfunc=()=>{
+console.log(x)
+}
+
+module.exports = ({x,xyzfunc})
